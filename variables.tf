1d116f3b4c84ecf5ca3287ad9dae040dce6840f5
Phong <phongmchau@gmail.com>
2025-09-28 22:01:48 +1000

    fmt

--- variables.tf
+++ variables.tf
@@ -1,27 +1,27 @@
 variable "resource_group_location" {
-    default = "rg-sandbox"
+  default = "rg-sandbox"
 }
 
 variable "agent_count" {
-    default = 3
+  default = 3
 }
 
 variable "ssh_public_key" {
-    default = "~/.ssh/id_rsa.pub"
+  default = "~/.ssh/id_rsa.pub"
 }
 
 variable "dns_prefix" {
-    default = "changeme"
+  default = "changeme"
 }
 
-variable cluster_name {
-      default = "changeme"
+variable "cluster_name" {
+  default = "changeme"
 }
 
-variable aks_service_principal_app_id {
-    default = "changeme"
+variable "aks_service_principal_app_id" {
+  default = "changeme"
 }
 
-variable aks_service_principal_client_secret {
-    default = "changeme"
+variable "aks_service_principal_client_secret" {
+  default = "changeme"
 }
